952aebf9bb5088ece1277888b45422817468500d
Julio Robles <Jucer74@Hotmail.com>
2024-06-25 15:20:27 -0500

    Initial Test

---
+++ KafkaTest/Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+#region Constants
+
+const string Topic = "purchases";
+
+#endregion Constants
+
+Console.WriteLine("...Consumer Started...");
+
+#region Consumer-Config
+
+// TODO : Complete Customer Config
+
+#endregion Consumer-Config
+
+#region Consumer-Operations
+
+// TODO : Complete Customer Operations
+
+#endregion Consumer-Operations
+
+Console.WriteLine("...Consumer Stoped...");
---
+++ KafkaTest/Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+
+#region Constants
+
+const string Topic = "purchases";
+const int NumMessages = 10;
+
+#endregion Constants
+
+#region Global-Values
+
+string[] users = { "rdowney", "jsmith", "bgriffin", "mjordan", "cgarcia", "awhite" };
+string[] items = { "book", "pencil", "ruler", "eraser", "notebook" };
+
+#endregion Global-Values
+
+Console.WriteLine("...Producer Started...");
+
+#region Producer-Config
+
+// TODO: Complete Producer Config
+
+#endregion Producer-Config
+
+#region Producer-Operations
+
+// TODO: Complete Producer Operations
+
+#endregion Producer-Operations
+
+Console.WriteLine("...Producer Stoped...");
